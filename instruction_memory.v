3271b6aab8eaa7ab52ac766ea93fc1b3828339e6
amlana250 <74863804+amlana250@users.noreply.github.com>
2021-07-07 13:27:21 +0530

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,12 +1,11 @@
-module instruction_memory (format,address,clk);
+module instruction_memory (format,address);
   output reg [31:0] format;
   input [31:0] address;
-  input clk;
+  /*input clk;*/
   reg [7:0] location [0:511];
 
-  always @ ( posedge clk ) begin
+  always @ ( * ) begin
     $readmemb("C:/Python_Verilog/RP_PROJECT/instruction_mem.txt", location);
     format={location[address],location[address+1],location[address+2],location[address+3]};
   end
 endmodule
-
